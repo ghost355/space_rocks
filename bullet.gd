61d901296550908ca220155883575b21976f42b7
Pavel Pavlov <kx355@mail.ru>
2025-02-16 15:42:56 +0300

    add shooting with laser sound

--- bullet.gd
+++ bullet.gd
@@ -16,3 +16,9 @@ func _process(delta: float):
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("rocks"):
+		body.explode()
+		queue_free()
--- player.gd
+++ player.gd
@@ -5,6 +5,11 @@ enum { INIT, ALIVE, INVULNERABLE, DEAD }
 @export var engine_power: int = 500
 @export var spin_power: int = 8000
 
+@export var bullet_scene: PackedScene
+@export var fire_rate: float = 0.25
+
+var can_shoot = true
+
 var thrust: Vector2 = Vector2.ZERO
 var rotation_dir: float = 0
 var state = INIT
@@ -13,11 +18,14 @@ var screensize = Vector2.ZERO
 
 @onready var sprite = $Sprite
 @onready var collision_shape = $CollisionShape2D
+@onready var gun_cooldown = $GunCooldown
+@onready var laser_sound = $LaserSound
 
 
 func _ready():
 	change_state(ALIVE)
 
+	gun_cooldown.wait_time = fire_rate
 	screensize = get_viewport_rect().size
 
 
@@ -48,6 +56,21 @@ func get_input():
 
 	rotation_dir = Input.get_axis("rotate_left", "rotate_right")
 
+	if Input.is_action_pressed("shoot") and can_shoot:
+		shoot()
+
+
+func shoot():
+	if state == INVULNERABLE:
+		return
+	laser_sound.play()
+	can_shoot = false
+	gun_cooldown.start()
+	
+	var b = bullet_scene.instantiate()
+	get_tree().root.add_child(b)
+	b.start($Muzzle.global_transform)
+
 
 func _physics_process(delta: float):
 	constant_force = thrust
@@ -59,3 +82,7 @@ func _integrate_forces(physics_state):
 	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
 	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
 	physics_state.transform = xform
+
+
+func _on_gun_cooldown_timeout() -> void:
+	can_shoot = true
